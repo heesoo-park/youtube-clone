72a6ec665eaef408b17becaa9519d3a63fae5935
heesoo <phs8505@gmail.com>
2021-07-30 12:35:15 +0900

    submitVideo

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import Dropzone from 'react-dropzone';
 import Axios from 'axios';
-//import { report } from '../../../../../server/routes/video';
+import { useSelector } from 'react-redux';
 
 const {TextArea} = Input;
 const { Title } = Typography;
@@ -19,8 +19,8 @@ const CategoryOptions = [
     {value: 3, label: "Pets & Animals"}
 ]
 
-function VideoUploadPage() {
-
+function VideoUploadPage(props) {
+    const user = useSelector(state => state.user);
     const [VideoTitle, setVideoTitle] = useState("")
     const [Description, setDescription] = useState("")
     const [Private, setPrivate] = useState(0)
@@ -82,21 +82,50 @@ function VideoUploadPage() {
             })
     }
 
+    const onSumit = (e) => {
+        e.preventDefault();
+
+        const variables = {
+            writer: user.userData._id,
+            title: VideoTitle,
+            description: Description,
+            privacy: Private,
+            filePath: FilePath,
+            category: Category,
+            duration: Duration,
+            thumbnail: ThumbnailPath,
+        }
+
+        Axios.post('/api/video/uploadVideo', variables)
+            .then(response => {
+                if(response.data.success) {
+                    
+                    message.success('성공적으로 업로드를 했습니다.')
+
+                    setTimeout(() => {
+                        props.history.push('/')
+                    }, 3000);
+                    
+                } else {
+                    alert('비디오 업로드에 실패했습니다.')
+                }
+            })
+    }
+
     return (
         <div style={{maxWidth:'700px', margin:'2rem auto'}}>
             <div style={{textAlign:'center', marginBottom:'2rem'}}>
                 <Title level={2}>Upload Video</Title>
             </div>
 
-            <Form onSubmit>
+            <Form onSubmit={onSumit}>
                 <div style={{display:'flex', justifyContent:'space-between'}}>
                     {/* Drop zone */}
                     
                     <Dropzone 
                         onDrop={onDrop}
                         multiple={false}
-                        maxSize={1000000000}
-                    >
+                        maxSize={1000000000}>
                         {({getRootProps, getInputProps}) => (
                             <div style={{width:'300px', height:'240px', border:'1px solid lightgray', display:'flex',
                             alignItems:'center', justifyContent:'center'}} {...getRootProps()}>
@@ -151,7 +180,7 @@ function VideoUploadPage() {
                 </select>
                 <br />
                 <br />
-                <Button type="primary" size="large" onClick>
+                <Button type="primary" size="large" onClick={onSumit}>
                     Submit
                 </Button>
 
