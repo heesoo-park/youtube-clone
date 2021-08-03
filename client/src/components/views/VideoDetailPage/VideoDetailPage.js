0d68648f72ecb05da19b13155e88c08f4bb84e2e
heesoo <phs8505@gmail.com>
2021-08-03 13:46:20 +0900

    sideVideos

--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import {Row, Col, List, Avatar} from 'antd';
 import Axios from 'axios';
+import SideVideo from './Sections/SideVideo';
 //import { response } from 'express';
 
 function VideoDetailPage(props) {
@@ -26,7 +27,6 @@ function VideoDetailPage(props) {
     if(VideoDetail.writer)
     {
         return (
-            <div>
                 <Row gutter={[16,16]}>
                     <Col lg={18} xs={24}>
                         <div style={{width:'100%', padding:'3rem 4rem'}}>
@@ -47,10 +47,9 @@ function VideoDetailPage(props) {
                         </div>
                     </Col>
                     <Col lg={6} xs={24}>
-                        Side Videos
+                        <SideVideo />
                     </Col>
                 </Row>
-            </div>
         )
     } else {
         return <div> ...loading </div>
